20e475e4a2240abe2265f23cf66650142c35ed36
Ming-beginner <lengocminh310808@gmail.com>
2022-01-19 22:41:27 +0700

    calc

---
+++ index.js
@@ -0,0 +1,44 @@
+$("document").ready(function () {
+  let expression = "";
+  let showInput = $(".input");
+  let showOutput = $(".output");
+  let answerBlock = $(".answer-block-container");
+  $(".keyboard-key").click(function (e) {
+    answerBlock.removeClass("active-block");
+    let textToAdd = $(e.target).val();
+    expression += textToAdd;
+    showInput.text(expression);
+  });
+  $(".keyboard-key-operator").click(function (e) {
+    answerBlock.removeClass("active-block");
+    let textToAdd = $(e.target).parent().val();
+    expression += textToAdd;
+    showInput.text(expression);
+  });
+  $(".keyboard-key-enter").click(function () {
+    let answer = eval(
+      expression
+        .split("")
+        .map((letter) => {
+          if (letter == "x") letter = "*";
+          if (letter == ":") letter = "/";
+          return letter;
+        })
+        .join("")
+    );
+    answerBlock.addClass("active-block");
+    showOutput.text(answer);
+    expression = "";
+    showInput.text("");
+  });
+  $(".keyboard-key-ac").click(function () {
+    expression = "";
+    showInput.text("");
+  });
+  $(".keyboard-key-del").click(function () {
+    let expressionList = expression.split("");
+    expressionList.pop();
+    expression = expressionList.join("");
+    showInput.text(expression);
+  });
+});
